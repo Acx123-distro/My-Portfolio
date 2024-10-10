fe05483ca8e83056f7abd8dbe363a0dcd5a804e9
Acx123-distro <boydistro6@gmail.com>
2024-10-10 15:18:39 +0100

    Worked on my portfolio

---
+++ Htm.js
@@ -0,0 +1,43 @@
+//if(10>2){
+  //  console.log();
+//}
+//else if(4<3){
+
+//}
+
+
+//10 > 2? "true": "false";<!--Ternary-->
+//<!--For Each-->
+//Myarrr.foreach(function(e){
+
+//})
+//<!--Map-->
+//myarr.map(e,function(){
+ //   return e;
+//})
+//<!--Filter-->
+//myarr.filter(e =>{
+  //return  e===2;
+//}
+
+
+
+//<!--Document Object Model-->
+// console.log(document) or(Window)
+// console.log(document.querySelector()) or querySelectorall
+// console.log(document.getElementsByClassName())
+// console.log(document.getElementById())
+
+// //<!--Event-->
+// const.myar
+
+// myar.addEventListener("Click",myar.style.background color;)
+// function myfun() {}
+
+// interpolation
+// `${}`
+// <Dart>
+// \'
+// \''
+// \$
+//"""how to print a string exactly as it is
